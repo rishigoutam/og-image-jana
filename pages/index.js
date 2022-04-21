77da4003e0247b9a214c1e57d38c97a94b68d19a
rg <rishigoutam@users.noreply.github.com>
2022-04-21 12:32:22 -0700

    preload fonts

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,62 @@
 /* eslint-disable @next/next/no-page-custom-font */
-import { useRouter } from 'next/dist/client/router';
-import Head from 'next/head';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const router = useRouter();
   const searchParams = new URLSearchParams(router.asPath.split(/\?/)[1]);
 
-  const title = searchParams.get('title');
-  const subtitle = searchParams.get('subtitle');
-  const tags = searchParams.get('tags');
+  const title = searchParams.get("title");
+  const subtitle = searchParams.get("subtitle");
+  const tags = searchParams.get("tags");
 
   const [params, setParams] = useState({
     title: null,
-    subtitle: null || '',
-    tags: null || '',
+    subtitle: null || "",
+    tags: null || "",
   });
 
   useEffect(() => {
     setParams({
       title,
       subtitle,
-      tags: tags || '',
+      tags: tags || "",
     });
   }, [title, subtitle, tags]);
 
   return (
-    <div className={'container'}>
+    <div className={"container"}>
       <Head>
-        <title>Rishi Goutam</title>
+        <title>Rishi Goutam OG-Image Generator | goutam.io</title>
+        <link
+          rel="preload"
+          href="/fonts/et-book/et-book-bold-line-figures/et-book-bold-line-figures.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/et-book/et-book-display-italic-old-style-figures/et-book-display-italic-old-style-figures.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/et-book/et-book-roman-line-figures/et-book-roman-line-figures.ttf"
+          as="font"
+          crossOrigin=""
+        />
       </Head>
 
       <div className="content">
         <h1>{params.title}</h1>
-        <h2><em>{params.subtitle}</em></h2>
+        <h2>
+          <em>{params.subtitle}</em>
+        </h2>
         <div className="tags">
-          {params.tags.split(',').map((tag) => {
+          {params.tags.split(",").map((tag) => {
             return <span key={tag}>#{tag}</span>;
           })}
         </div>
